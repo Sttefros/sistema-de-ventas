8ab80a7b1500de8cc23ce1a2201b58b40c22be09
EsteGall <es.gallardot@duocuc.cl>
2022-04-27 00:03:29 -0400

    V0.2

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 
 // config acceso DB
 define('DB_HOST', 'localhost');
-define('DB_USER', 'duoc');
+define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NOMBRE', 'proyecto');
 
@@ -12,6 +12,6 @@ define('DB_NOMBRE', 'proyecto');
 define('RUTA_APP', dirname(dirname(__FILE__)));
 
 //RUTA URL EJEMPLO: http://localhost/proyecto/
-define('RUTA_URL', 'http://localhost/proyecto');
+define('RUTA_URL', 'http://localhost/yuyitos2');
 
 define('NOMBRESITIO', '_NOMBRE_SITIO');
