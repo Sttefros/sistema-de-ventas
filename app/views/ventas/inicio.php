11cfc43b2d6d35ca711f42a8698eba61a6f6b28a
EsteGall <es.gallardot@duocuc.cl>
2022-05-20 16:25:50 -0400

    Update inicio.php

--- app/views/ventas/inicio.php
+++ app/views/ventas/inicio.php
@@ -28,7 +28,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Stock de Productos</h1>
+            <h1 class="m-0">Lista Ventas</h1>
           </div><!-- /.col -->
           
         </div><!-- /.row -->
