5d6309d250efa00553ebb6153f90f12d3cd8e00b
EsteGall <es.gallardot@duocuc.cl>
2022-05-26 12:19:23 -0400

    Revert "v 0.7.1"
    This reverts commit 59f9fe62f230a5645da199dae6ebd0eb8e5b5a76.

--- app/models/Cliente.php
+++ app/models/Cliente.php
@@ -74,7 +74,7 @@
 
 		public function ultimoCliente(){
 
-			$this->db->query("CALL ultimoCliente()");
+			$this->db->query("SELECT * FROM cliente ORDER BY id_cliente desc LIMIT 1");
 			return json_decode(json_encode($this->db->registro()), true);
 		}
 	}
--- app/models/Detalle_venta.php
+++ app/models/Detalle_venta.php
@@ -9,7 +9,7 @@
 
 		public function terminarVenta($datos_agregar, $id_venta){
 
-			$this->db->query("CALL terminarDetalleVenta(:id_venta,:id_producto,:cantidad_producto,:precio_producto,:precio_total_producto)");
+			$this->db->query("INSERT INTO detalle_venta (`id_venta`, `id_producto`, `cantidad_producto`, `precio_producto`, `precio_total_producto`) VALUES (:id_venta,:id_producto,:cantidad_producto,:precio_producto,:precio_total_producto)");
 
 			$this->db->bind(':id_venta', $id_venta, null);
 			$this->db->bind(':id_producto', $datos_agregar['id_producto'], null);
